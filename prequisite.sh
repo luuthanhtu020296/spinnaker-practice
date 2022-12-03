105bce856a11f274400a6becff793c5b93af2ebb
luuthanhtu <hoangtusocolakuteboy@gmail.com>
2022-12-04 03:31:27 +0700

    Update prequisite.sh

--- prequisite.sh
+++ prequisite.sh
@@ -20,5 +20,6 @@ sudo apt install -y kubelet kubeadm kubectl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install -y unzip
 unzip awscliv2.zip
 sudo ./aws/install
