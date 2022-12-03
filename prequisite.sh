811da0a6d21afe0e256b926c45e7b23caa3f84bf
luuthanhtu <hoangtusocolakuteboy@gmail.com>
2022-12-03 22:34:07 +0700

    Update prequisite.sh

--- prequisite.sh
+++ prequisite.sh
@@ -1,5 +1,7 @@
 #!/bin/bash#
 
+# install docker
+
 sudo apt update -y
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
@@ -7,3 +9,17 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docke
 sudo apt update -y
 sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
+
+# install kubenetenes
+
+sudo su -
+ufw disable
+swapoff -a; sed -i '/swap/d' /etc/fstab
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+apt update && apt install -y kubeadm kubelet kubectl
