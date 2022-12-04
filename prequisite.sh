32de32f258895a592ea78e2c7ce4230fe86bb933
luuthanhtu <hoangtusocolakuteboy@gmail.com>
2022-12-04 20:02:33 +0700

    Update prequisite.sh

--- prequisite.sh
+++ prequisite.sh
@@ -1,15 +1,17 @@
 #!/bin/bash#
 
+sudo -su
+export HOME=/home/spinnaker
 # install docker
 
-sudo apt update -y
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update -y
-sudo apt install -y docker-ce
-sudo usermod -aG docker ${USER}
-sudo chmod 777 /var/run/docker.sock
+# sudo apt update -y
+# sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt update -y
+# sudo apt install -y docker-ce
+# sudo usermod -aG docker ${USER}
+# sudo chmod 777 /var/run/docker.sock
 # install kubenetenes
 
 sudo apt install -y apt-transport-https curl
@@ -38,5 +40,34 @@ sudo bash InstallHalyard.sh
 
 
 #create namespace
+sudo aws configure set aws_access_key_id $ACCESS_KEY
+sudo aws configure set aws_secret_access_key $SECRET_KEY
+sudo aws configure set default.region us-west-2
+eksctl create cluster --name=eks-spinnaker --nodes=2 --region=us-west-2 --write-kubeconfig=false
+aws eks update-kubeconfig --name eks-spinnaker --region us-west-2 --alias eks-spinnaker
 kubectl create namespace spinnaker
+hal config provider kubernetes enable
+kubectl config use-context eks-spinnaker
+CONTEXT=$(kubectl config current-context --kubeconfig "/home/spinnaker/.kube/config")
+kubectl apply --context $CONTEXT -f service-account.yaml
+TOKEN=$(kubectl get secret --context $CONTEXT \
+   $(kubectl get serviceaccount spinnaker-service-account \
+       --context $CONTEXT \
+       -n spinnaker \
+       -o jsonpath='{.secrets[0].name}') \
+   -n spinnaker \
+   -o jsonpath='{.data.token}' | base64 --decode)
+kubectl config set-credentials ${CONTEXT}-token-user --token $TOKEN
+kubectl config set-context $CONTEXT --user ${CONTEXT}-token-user
 hal config provider kubernetes account add eks-spinnaker --kubeconfig-file "/home/spinnaker/.kube/config" --context $(kubectl config current-context --kubeconfig "/home/spinnaker/.kube/config")
+hal config features edit --artifacts true
+hal config deploy edit --type distributed --account-name eks-spinnaker
+hal config storage s3 edit \
+ --access-key-id $ACCESS_KEY \
+ --secret-access-key $SECRET_KEY --region us-west-2
+hal config storage edit --type s3
+export VERSION=1.19.2
+hal config version edit --version $VERSION
+hal -l DEBUG deploy apply
+# eksctl delete cluster --name eks-spinnaker
+
